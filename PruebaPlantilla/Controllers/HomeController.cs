f93582a70081d47fe29c4b2c13e161a955fb2bdf
Francisco Javier Sandoval Maldonado <francisco.sandoval@intra.juegaok.com>
2022-11-22 14:30:47 -0600

    Inicio de plantilla dinamica

---
+++ PruebaPlantilla/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+namespace PruebaPlantilla.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AnotherLink()
+        {
+            return View("Index");
+        }
+
+        public ActionResult FirstTab()
+        {
+            return PartialView("_FirstTab");
+        }
+
+        public ActionResult SecondTab()
+        {
+            //return PartialView("_SecondTab");
+            return PartialView("_FirstTab");
+        }
+    }
+}
---
+++ PruebaPlantilla/Scripts/Plantilla.js
@@ -0,0 +1,179 @@
+﻿let tabCounter = 1;
+var contenedorTabNodos = $('#tabNodos');
+var c = {
+    contenedorTab: $('#tabContenedor'),
+    TabItems: $('#tabNodos'),
+    scrollBarWidths: 40
+}
+
+$('#add_tab').click(function (id, name, href, descripcion, ruta) {
+    name = "prueba" + tabCounter;
+    tabCounter += 1;
+    id = tabCounter;
+    descripcion = "esto es una prueba";
+
+
+    c.contenedorTab.find('div.tab-general').siblings().removeClass('show');
+    c.contenedorTab.find('div.tab-general').first().removeClass('show active');
+    c.contenedorTab.find('div.tab-general').siblings().removeClass('active');
+
+    c.TabItems.find('li').removeClass('active').attr('aria-selected', 'false');
+
+
+    var li = $('<li></li>').addClass('nav-item active')
+    var a = $('<a></a>').addClass('nav-link').html(name)
+    a.attr('href', '#div_' + id)
+    a.attr('aria-controls', 'div_' + id)
+    a.attr('id', id);
+    a.attr('data-toggle', 'tab');
+    a.attr('role', 'tab')
+    a.attr('aria-selected', 'true');
+    a.attr('data-descripcion', descripcion);
+    a.attr('data-ruta', ruta);
+    a.attr('data-accion', ".");
+    a.append('<span  class="tabAction la la-sync refreshTab"></span>')
+    a.append('<span class="la la-close tabAction closeTab"></span>')
+
+    li.append(a)
+    c.TabItems.append(li)
+
+    var div = $('<div></div>').addClass("tab-pane tab-general active");
+    div.attr('role', 'tabpanel');
+    div.attr('id', "div_" + id);
+    div.attr('aria-labelledby', id)
+    div.html('<h2>' + "hola" + tabCounter + '</h2>')
+    c.contenedorTab.append(div)
+
+    //var ul = tabs.find('ul');
+    //tabCounter = ul.length + 1;
+    //alert(tabCounter);
+    //$("<li><a href='#" + tabCounter + "'>New Tab</a> </li>").appendTo(ul);
+    //$("<div id='" + tabCounter + "'>HOLA</div>").appendTo(tabs);
+    //tabs.tabs("refresh");
+    //tabs.tabs("option", "active", 1);
+
+});
+
+contenedorTabNodos.on('click', 'span.closeTab', function () {
+    CloseTab($(this))
+});
+
+contenedorTabNodos.on('click', 'span.refreshTab', function () {
+    //refreshTabEvent($(this))
+});
+
+function CloseTab (t) {
+    var $this = t;
+    var a = $this.closest('a');
+    var idTab = a.attr('href');
+    var index = $this.parent('a').parent('li').index();
+
+    $('#Ruta').text('');
+    $('#AccionFormulario').text('');
+
+    a.closest('li').remove();
+    c.contenedorTab.find(idTab).remove();
+    if (c.TabItems.find('li').length > 0) {
+        if (index > 0) {
+            var nodo = $("#tabNodos li").eq(index - 1);
+
+            var Existe_Elemento = $(nodo).find('a').attr('href');
+            c.TabItems.find('li.active').removeClass('active');
+            c.contenedorTab.find('div').first().removeClass('active show');
+            // Si el elemento existe agregar clase active.
+            $(Existe_Elemento).addClass('active');
+
+            $("a[href='" + Existe_Elemento + "']").parent('li').addClass('active');
+            $('#Ruta').text($("a[href='" + Existe_Elemento + "']").data('ruta'));
+            $('#AccionFormulario').text($("a[href='" + Existe_Elemento + "']").data('accion'));
+
+
+        } else {
+            var nodo = c.TabItems.find("li").eq(index);
+            var Existe_Elemento = $(nodo).find('a').attr('href');
+            c.TabItems.find('li.active').removeClass('active');
+            c.contenedorTab.find('div').first().removeClass('active show');
+            // Si el elemento existe agregar clase active.
+            $(Existe_Elemento).addClass('active show');
+            var elementoTemp = $("a[href='" + Existe_Elemento + "']");
+            $("a[href='" + Existe_Elemento + "']").parent('li').addClass('active');
+        }
+    }
+
+};
+
+function refreshTabEvent (t) {
+    var receptorhtml = null;
+    var baseUrl = "Home";
+    var _this = t;
+    var link = _this.closest('a');
+    var idFormulario = link.attr('href').substring(5);
+    var url = $('.main-menu').find('[data-id="' + idFormulario + '"]').attr('data-url')
+
+    //$.get(baseUrl + url, function (data) {
+        receptorhtml = data;
+    //}).fail(function (data) {
+    //    switch (data.status) {
+    //        case 500:
+    //            receptorhtml = error500();
+    //            break;
+    //        case 404:
+    //            receptorhtml = error404();
+    //            break;
+    //        default:
+
+    //    //}
+    //}).always(function () {
+    //    $("#div_" + idFormulario).html(receptorhtml);
+    //    $("#div_" + idFormulario).find('table.table').addClass('table-striped')
+    //});
+};
+
+function error500 () {
+    return '<center style="width: 31%; margin: 0 auto;margin-top: 10px;">' +
+        '<div class="m-portlet m-portlet--tab">' +
+        '<div class="m-portlet__head">' +
+        '<div class="m-portlet__head-caption">' +
+        '<div class="m-portlet__head-title">' +
+        '<span class="m-portlet__head-icon m--hide">' +
+        '<i class="la la-gear"></i>' +
+        '</span>' +
+        '<h3 class="m-portlet__head-text">' +
+        'Error 505 - Looks like we are having some server issues.' +
+        '</h3>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '<div class="m-portlet__body">' +
+        ' Go back to the previous page and try again.' +
+        '</br>' +
+        'If you think something is broken, report a problem.' +
+        '</div>' +
+        '</div>' +
+        '</center>'
+
+};
+
+function error404() {
+    return '<center style="width: 31%; margin: 0 auto;margin-top: 10px;">' +
+        '<div class="m-portlet m-portlet--tab">' +
+        '<div class="m-portlet__head">' +
+        '<div class="m-portlet__head-caption">' +
+        '<div class="m-portlet__head-title">' +
+        '<span class="m-portlet__head-icon m--hide">' +
+        '<i class="la la-gear"></i>' +
+        '</span>' +
+        '<h3 class="m-portlet__head-text">' +
+        'Error 404 - the page you are looking for does not exist.' +
+        '</h3>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '<div class="m-portlet__body">' +
+        'You may want to head back to the homepage.' +
+        '</br>' +
+        'If you think something is broken, report a problem.' +
+        '</div>' +
+        '</div>' +
+        '</center>'
+};
